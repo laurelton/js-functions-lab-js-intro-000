c960bf92acccf1174d3deb72283ae78bef92eb70
Frank Rose <frankrosework@gmail.com>
2018-03-28 12:23:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // write your code below!
+var happyHolidays = function() {
+  return 'Happy holidays!';
+};
+
+var happyHolidaysTo = function(name, holiday='Happy holidays') {
+  return '';
+};
+
+var holidayCountdown = function() {};
